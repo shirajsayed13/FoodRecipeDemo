400e92e88601acee7ea749962d59c1dd48992864
Shiraj Sayed <shirajsayed13@gmail.com>
2019-10-05 13:13:51 +0530

    Add Retrofit Interface method

--- app/src/main/java/com/shirajsayed/foodrecipe/requests/RecipeApi.java
+++ app/src/main/java/com/shirajsayed/foodrecipe/requests/RecipeApi.java
@@ -1,7 +1,29 @@
 package com.shirajsayed.foodrecipe.requests;
 
+import com.shirajsayed.foodrecipe.requests.reponses.RecipeResponse;
+import com.shirajsayed.foodrecipe.requests.reponses.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 /**
  * @author Shiraj Sayed
  */
 public interface RecipeApi {
+
+    //Search
+    @GET("api/search")
+    Call<RecipeSearchResponse> searchRecipe(
+            @Query("key") String key,
+            @Query("q") String query,
+            @Query("page") String page
+    );
+
+    // GET RECIPE REQUEST
+    @GET("api/get")
+    Call<RecipeResponse> recipe(
+            @Query("key") String key,
+            @Query("rId") String id
+    );
 }
