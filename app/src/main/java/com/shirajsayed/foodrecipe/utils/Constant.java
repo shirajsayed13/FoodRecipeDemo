c629ff299d45533bc65483b9a374b153bc5e51cd
Shiraj Sayed <shirajsayed13@gmail.com>
2019-10-05 12:07:25 +0530

    Add BaseUrl and API_KEY

---
+++ app/src/main/java/com/shirajsayed/foodrecipe/utils/Constant.java
@@ -0,0 +1,16 @@
+package com.shirajsayed.foodrecipe.utils;
+
+/**
+ * @author Shiraj Sayed
+ */
+public class Constant {
+
+    public static final String BASE_URL = "https://www.food2fork.com/";
+
+    /**
+     * GET YOUR OWN API KEY
+     * https://www.food2fork.com/about/api
+     */
+    public static final String API_KEY = "07d2885014ef642b03e84d2a78f4ab19";
+
+}
